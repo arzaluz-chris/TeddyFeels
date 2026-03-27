01cdf3802d96716b8d7d2f343a56903be45498de
arzaluz-chris <23008381@es.uveg.edu.mx>
2026-03-27 17:15:51 -0600

    Initial Commit

---
+++ TeddyFeels/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TeddyFeels
+//
+//  Created by Christian Arzaluz on 27/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TeddyFeels/TeddyFeelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeddyFeelsApp.swift
+//  TeddyFeels
+//
+//  Created by Christian Arzaluz on 27/03/26.
+//
+
+import SwiftUI
+
+@main
+struct TeddyFeelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
